19f1c24a6b5df9d0d5bc41c95b6c51e43f59afd9
SantiagoGM19 <santigm1905@gmail.com>
2022-06-08 23:39:04 -0500

    mongodb database configured

---
+++ src/main/java/com/sofkau/enterpriseAppBackEnd/EnterpriseAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.enterpriseAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppBackEndApplication.class, args);
+	}
+
+}
