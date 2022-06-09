388b60b0fc2cd2976fceaec0f997ae098fd83a2d
SantiagoGM19 <santigm1905@gmail.com>
2022-06-09 15:02:03 -0500

    get all products router created

--- src/main/java/com/sofkau/enterpriseAppBackEnd/routes/ProductRouter.java
+++ src/main/java/com/sofkau/enterpriseAppBackEnd/routes/ProductRouter.java
@@ -1,12 +1,17 @@
 package com.sofkau.enterpriseAppBackEnd.routes;
 
 import com.sofkau.enterpriseAppBackEnd.dto.ProductDTO;
+import com.sofkau.enterpriseAppBackEnd.usecases.ProductUseCases.GetAllProductsUseCase;
 import com.sofkau.enterpriseAppBackEnd.usecases.ProductUseCases.SaveProductUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -15,7 +20,7 @@ public class ProductRouter {
 
     @Bean
     public RouterFunction<ServerResponse> saveProductRouter(SaveProductUseCase saveProductUseCase){
-        return route(POST("/save/product").and(accept(MediaType.APPLICATION_JSON)),
+        return route(POST("/products").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(ProductDTO.class)
                         .flatMap(productDTO -> saveProductUseCase.apply(productDTO))
                         .flatMap(result ->
@@ -23,5 +28,14 @@ public class ProductRouter {
                                         .contentType(MediaType.APPLICATION_JSON)
                                         .bodyValue(result))
         );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllProductsRouter(GetAllProductsUseCase getAllProductsUseCase){
+        return route(GET("/products"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllProductsUseCase.get(), ProductDTO.class))
+                );
     }
 }
